0901d385f3aa852df6af8c73e911dc17433e75fb
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-04-30 12:29:06 -0700

    finished

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import json
 
 # read `expenses.json`
+with open ("expenses.json", "r") as f:
+    data = json.load(f)
 
 # get and print total "price" for all expenses at the "pet store"
+prices_total = 0
+new_dict = data["pet store"]
+for key in new_dict:
+    prices_total += key["price"]
+print(prices_total)
